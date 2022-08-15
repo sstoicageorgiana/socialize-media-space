b8cc2d75732799bc653b5f80682bd5d988851ae3
sstoicageorgiana <60180195+sstoicageorgiana@users.noreply.github.com>
2022-08-15 03:02:30 +0300

    - new react app - new script for heroku postbuild used to render react app on cloud => Heroku -app.js config for production enviroment - dev script using concurrently package for starting both on client and server local dev machene
    - new react app
    - new script for heroku postbuild used to render react app on cloud => Heroku
    -app.js config for production enviroment
    - dev script using concurrently package for starting both on client and server local dev machene

--- app.js
+++ app.js
@@ -13,19 +13,32 @@ const app = express();
 app.use(bodyParser.json());
 
 //#region Routes
-app.get('/', (req, res)=> res.send('Success! Social media app hosted!')); //test route
-app.use('/api/auth', authRouter);                              // =>>I want when having '/api/auth' to use authRouter(if we have in auth.js GET, POST, PUT, DELETE it will go to the " ('/') " and use the "/" and then come back and added the following path: '/api/auth'(this is not about the pathfile) to fo forewword )
+app.get('/', (req, res)=> res.send('Success! Social media app hosted!')); 
+app.use('/api/auth', authRouter);                              
 app.use('/api/posts',postsRouter);
 app.use('/api/users', userRouter);
 //#endregion
+
 
 //#region ListeningToPort
 app.listen(process.env.PORT ||5000) 
 //#endregion
 
+
 //#region Connect to MongoDB
-// console.log(process.env.MONGO_URI);
-// mongoose.connect(process.env.MONGO_URI, () => console.log("connected to mongodb"))
+
+// process.env.NODE_ENV  it is set up by mongodb
+if(process.env.NODE_ENV === 'production'){
+	//ce dorin sa dam deploy => frontend 
+	app.use(express.static('client/build'));
+	//react are script care se numeste build => npm run build=> se creaza un folder build cu codul minificat intr-un fisier index.html
+	app.get( '*', (req, res) => {
+		req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+	});
+} //remeber: react build duce intr-un folder build, un fisier index.html care va fi pct care deschide app
+
+//trebui sa ii spunem cum sa faca build
+
 
 const connectDB = async () => {
 	try {
@@ -33,13 +46,14 @@ const connectDB = async () => {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
-
 		console.log('MongoDB connected!!');
 	} catch (err) {
 		console.log('Failed to connect to MongoDB', err);
 	}
 };
 connectDB();
+
+
 //#endregion
 
 
