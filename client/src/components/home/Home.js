52394815e3dab32b19467a13062375983a126f65
sstoicageorgiana <60180195+sstoicageorgiana@users.noreply.github.com>
2022-08-18 22:39:29 +0300

    Corrected Issue on Home.js
    bug -Typo in Home.js

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Home = (props) => {
-    return <div> Welcome</div>;
-}
+	return <div>Home New</div>;
+};
 
-Home.PropTypes = {};
+Home.propTypes = {};
 
 export default Home;
